a6016214db04a558465a0b87c4bfc714577facab
xppazarr <79111364+xppazarr@users.noreply.github.com>
2024-03-29 06:38:13 +0300

    Update Add GM Tag to YOUR username..user.js
    Fixed font

--- Add GM Tag to YOUR username..user.js
+++ Add GM Tag to YOUR username..user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Add GM Tag to YOUR username.
+// @name         Add GM Tag to Usernames
 // @namespace    http://tampermonkey.net/
 // @version      1
-// @description  Adds a GM tag to you / Works on computer games aswell.
-// @author       Solar / discord sola.r & https://www.chess.com/member/solarmt
+// @description  Adds a GM tag to you
+// @author       Solar & discord sola.r & https://www.chess.com/member/solarmt
 // @include      https://www.chess.com/*
 // @grant        none
 // ==/UserScript==
@@ -27,11 +27,46 @@
             gmTag.setAttribute('data-tooltip-target', '12');
             gmTag.textContent = 'GM';
 
+             // Apply styling to make the GM tag bigger
+        gmTag.style.fontSize = '13px'; // Adjust as needed
+
             // Insert the GM tag element as the first child of the user tagline component
             userTaglineComponent.insertBefore(gmTag, userTaglineComponent.firstChild);
         }
     }
+
+    // Find the profile card user block
+    var profileCardUserBlock = document.querySelector('div.profile-card-user-block');
+
+    if (profileCardUserBlock) {
+        // Create the GM tag element
+        var gmTagProfile = document.createElement('a');
+        gmTagProfile.setAttribute('class', 'profile-card-chesstitle');
+        gmTagProfile.setAttribute('href', 'https://www.chess.com/members/titled-players');
+        gmTagProfile.setAttribute('v-tooltip', 'Grandmaster');
+        gmTagProfile.setAttribute('data-tooltip-target', '0');
+        gmTagProfile.textContent = 'GM';
+
+        // Insert the GM tag element as the first child of the profile card user block
+        profileCardUserBlock.insertBefore(gmTagProfile, profileCardUserBlock.firstChild);
+    }
+
+    // Find the home user avatar
+    var homeUserAvatar = document.querySelector('.home-user-avatar');
+
+    if (homeUserAvatar) {
+        // Create the GM tag element
+        var gmTagHome = document.createElement('a');
+        gmTagHome.setAttribute('class', 'user-chess-title-component');
+        gmTagHome.setAttribute('href', 'https://www.chess.com/members/titled-players');
+        gmTagHome.setAttribute('v-tooltip', 'Grandmaster');
+        gmTagHome.setAttribute('data-tooltip-target', '12');
+        gmTagHome.textContent = 'GM';
+
+         // Apply styling to make the GM tag bigger
+        gmTagHome.style.fontSize = '15px'; // Adjust as needed
+
+        // Insert the GM tag element after the home user avatar
+        homeUserAvatar.parentNode.insertBefore(gmTagHome, homeUserAvatar.nextSibling);
+    }
 })();
-
-
-// If
